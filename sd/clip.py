a95954fe085403586b924a874f11c172e08cff6b
Kyler <kylerrosen@gmail.com>
2024-04-03 21:19:59 -0400

    had to update a ton of notation because of issues with importing the stable diffusion weights

--- sd/clip.py
+++ sd/clip.py
@@ -5,47 +5,71 @@ from attention import SelfAttention
 
 
 class CLIPEmbedding(nn.Module):
-    def __init__(self, vocab_size: int, n_embed: int, n_special_tokens: int):
+    def __init__(self, n_vocab: int, n_embd: int, n_token: int):
         super().__init__()
 
-        self.token_embedding = nn.Embedding(vocab_size, n_embed)
-        self.position_embedding = nn.Parameter(torch.zeros(n_special_tokens, n_embed))
+        self.token_embedding = nn.Embedding(n_vocab, n_embd)
+        # A learnable weight matrix encodes the position information for each token
+        self.position_embedding = nn.Parameter(torch.zeros((n_token, n_embd)))
 
-    def forward(self, tokens: torch.LongTensor) -> torch.Tensor:
+    def forward(self, tokens):
+        # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
         x = self.token_embedding(tokens)
+        # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+        x += self.position_embedding
 
-        return x + self.position_embedding
+        return x
 
 
 class CLIPLayer(nn.Module):
-    def __init__(self, n_heads: int, n_embed: int):
+    def __init__(self, n_head: int, n_embd: int):
         super().__init__()
 
-        self.layernorm1 = nn.LayerNorm(n_embed)
-        self.attention = SelfAttention(n_heads, n_embed)
-        self.layernorm2 = nn.LayerNorm(n_embed)
+        # Pre-attention norm
+        self.layernorm_1 = nn.LayerNorm(n_embd)
+        # Self attention
+        self.attention = SelfAttention(n_head, n_embd)
+        # Pre-FNN norm
+        self.layernorm_2 = nn.LayerNorm(n_embd)
+        # Feedforward layer
+        self.linear_1 = nn.Linear(n_embd, 4 * n_embd)
+        self.linear_2 = nn.Linear(4 * n_embd, n_embd)
 
-        self.linear1 = nn.Linear(n_embed, n_embed * 4)
-        self.linear2 = nn.Linear(n_embed * 4, n_embed)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
+        # (Batch_Size, Seq_Len, Dim)
         residue = x
 
-        x = self.layernorm1(x)
-        x = self.attention(x)
+        ### SELF ATTENTION ###
 
-        x = residue + x
+        # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim)
+        x = self.layernorm_1(x)
+
+        # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim)
+        x = self.attention(x, causal_mask=True)
+
+        # (Batch_Size, Seq_Len, Dim) + (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim)
+        x += residue
+
+        ### FEEDFORWARD LAYER ###
+        # Apply a feedforward layer where the hidden dimension is 4 times the embedding dimension.
 
         residue = x
+        # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim)
+        x = self.layernorm_2(x)
 
-        x = self.layernorm2(x)
-        x = self.linear1(x)
+        # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, 4 * Dim)
+        x = self.linear_1(x)
 
-        x = x * torch.sigmoid(1.702 * x)  # QuickGELU
+        # (Batch_Size, Seq_Len, 4 * Dim) -> (Batch_Size, Seq_Len, 4 * Dim)
+        x = x * torch.sigmoid(1.702 * x)  # QuickGELU activation function
 
-        x = self.linear2(x)
+        # (Batch_Size, Seq_Len, 4 * Dim) -> (Batch_Size, Seq_Len, Dim)
+        x = self.linear_2(x)
 
-        return residue + x
+        # (Batch_Size, Seq_Len, Dim) + (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim)
+        x += residue
+
+        return x
 
 
 class CLIP(nn.Module):
@@ -54,17 +78,22 @@ class CLIP(nn.Module):
         self.embedding = CLIPEmbedding(49408, 768, 77)
 
         self.layers = nn.ModuleList([
-            CLIPLayer(12, 768) for _ in range(12)
+            CLIPLayer(12, 768) for i in range(12)
         ])
 
         self.layernorm = nn.LayerNorm(768)
 
-    def forward(self, tokens: torch.LongTensor) -> torch.Tensor:
+    def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
         tokens = tokens.type(torch.long)
 
+        # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
         state = self.embedding(tokens)
 
+        # Apply encoder layers similar to the Transformer's encoder.
         for layer in self.layers:
+            # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim)
             state = layer(state)
+        # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim)
+        output = self.layernorm(state)
 
-        return self.layernorm(state)
+        return output
--- sd/model_loader.py
+++ sd/model_loader.py
@@ -5,75 +5,24 @@ from diffusion import Diffusion
 
 import model_converter
 
-
 def preload_models_from_standard_weights(ckpt_path, device):
     state_dict = model_converter.load_from_standard_weights(ckpt_path, device)
 
-    def fix_state_dict(s_dict):
-        new_state_dict = {}
-        for key, value in s_dict.items():
-            # Replace incorrect key parts with the correct ones
-            new_key = key
-
-            new_key = key.replace('residual_layer', 'residual')
-            new_key = new_key.replace('groupnorm_', 'groupnorm')
-            new_key = new_key.replace('conv_', 'conv')
-            # new_key = new_key.replace('residual_', 'residual')
-            new_key = new_key.replace('attention_', 'attention.')
-
-            # Fix the missing dots between the block number and the layer name
-            parts = new_key.split('.')
-            new_parts = []
-            for part in parts:
-                if part.isnumeric() and new_parts:
-                    new_parts[-1] = new_parts[-1] + '.' + part
-                else:
-                    new_parts.append(part)
-            new_key = '.'.join(new_parts)
-
-            new_state_dict[new_key] = value
-
-        return new_state_dict
-
     encoder = VAE_Encoder().to(device)
-    encoder_state_dict = fix_state_dict(state_dict["encoder"])
-    encoder.load_state_dict(encoder_state_dict, strict=True)
+    encoder.load_state_dict(state_dict['encoder'], strict=True)
 
     decoder = VAE_Decoder().to(device)
-    decoder_state_dict = fix_state_dict(state_dict["decoder"])
-    decoder.load_state_dict(decoder_state_dict, strict=True)
-
-    def fix_state_dict_diffusion(s_dict):
-        new_state_dict = {}
-        for key, value in s_dict.items():
-            new_key = key.replace('_', '.')  # Initial replacement to convert underscores to dots
-            # Specific replacements based on the observed naming patterns
-            new_key = new_key.replace('linear.time', 'linear_time')
-            new_key = new_key.replace('conv.feature', 'conv1_feature')
-            new_key = new_key.replace('conv.merged', 'conv2_merged')
-            new_key = new_key.replace('linear.linear_geglu', 'linear_geglu')
-            new_key = new_key.replace('attention.out.proj', 'attention1.out_proj')
-            new_key = new_key.replace('attention.in.proj', 'attention1.in_proj')
-            new_key = new_key.replace('attention.q.proj', 'attention2.q_proj')
-            new_key = new_key.replace('attention.k.proj', 'attention2.k_proj')
-            new_key = new_key.replace('attention.v.proj', 'attention2.v_proj')
-            new_key = new_key.replace('residual.layer', 'residual')
-
-            new_state_dict[new_key] = value
-
-        return new_state_dict
+    decoder.load_state_dict(state_dict['decoder'], strict=True)
 
     diffusion = Diffusion().to(device)
-    diffusion_state_dict = fix_state_dict_diffusion(state_dict["diffusion"])
-    diffusion.load_state_dict(diffusion_state_dict, strict=True)
+    diffusion.load_state_dict(state_dict['diffusion'], strict=True)
 
     clip = CLIP().to(device)
-    clip_state_dict = fix_state_dict(state_dict["clip"])
-    clip.load_state_dict(clip_state_dict, strict=True)
+    clip.load_state_dict(state_dict['clip'], strict=True)
 
     return {
-        "clip": clip,
-        "encoder": encoder,
-        "decoder": decoder,
-        "diffusion": diffusion,
+        'clip': clip,
+        'encoder': encoder,
+        'decoder': decoder,
+        'diffusion': diffusion,
     }
